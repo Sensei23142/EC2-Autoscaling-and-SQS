3cdad078871e01dedbbd5f8a861cd71d7befd936
Sensei23142 <148989306+Sensei23142@users.noreply.github.com>
2024-01-26 19:26:05 +0100

    Update UserDataEc2.bash

--- UserDataEc2.bash
+++ UserDataEc2.bash
@@ -5,45 +5,40 @@ sudo python3 -m pip install boto3
 
  
  
-cat <<EOF > /home/ec2-user/Consumer.py 
- 
-#!/usr/bin/python3 
- 
+cat <<EOF > /home/ec2-user/Consumer.py
+
+#!/usr/bin/python3
+
 import boto3
 import time
 import json
 
-
 sqs = boto3.client('sqs', region_name='us-east-1')
-
-
 queue_url = 'https://sqs.us-east-1.amazonaws.com/654654419570/MyQueue'
 
 while True:
-    
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=['All'],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=['All'],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=10  
-    )
+    try:
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=['All'],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=['All'],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=10
+        )
 
-    if 'Messages' in response:
-        for message in response['Messages']:
-            
-            content = json.loads(message['Body'])
-            print(f"Processing message: {content}")
+        if 'Messages' in response:
+            for message in response['Messages']:
+                content = json.loads(message['Body'])
+                print(f"Processing message: {content}")
+                receipt_handle = message['ReceiptHandle']
+                sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
-         
-            receipt_handle = message['ReceiptHandle']
-            sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
-
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
     time.sleep(60)
-
-EOF 
+EOF
  
  
 chmod +x /home/ec2-user/Consumer.py 
